e9ff055e6317322bdadf6ea291d1dfd456ca3f12
shaharkazaz <shahar.kazaz@gmail.com>
2021-10-17 12:38:22 +0300

    chore: copy readme images to dist

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -5,6 +5,10 @@ copyAssets();
 function copyAssets() {
     const {scripts, devDependencies, ['lint-staged']: _, config, husky, ...cleanPackage} = JSON.parse(fs.readFileSync('package.json').toString());
     fs.writeFileSync('dist/package.json', JSON.stringify(cleanPackage, null, 2));
-    fs.copyFileSync('README.md', 'dist/README.md');
+
+    const assets = ['README.md', 'step-1.png', 'step-2.png'];
+    for (const asset of assets) {
+        fs.copyFileSync(asset, `dist/${asset}`);
+    }
 }
 
